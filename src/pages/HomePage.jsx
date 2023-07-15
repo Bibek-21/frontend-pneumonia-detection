36d77fbbde00961f636078eda0d1ac830c01b656
bibek regmi <bregmi06@gmail.com>
2023-07-15 10:42:12 +0545

    react done hallka fulka

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,49 @@
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import HomeHeader from "../assets/image.svg";
+
+// function HomePage() {
+//   return (
+//     <div className="container text-center">
+//       <div className="row">
+//         <div className="col d-flex flex-column justify-content-center align-items-center">
+//           <h1 className="mt-5 text-uppercase">Pneumonia Detection</h1>
+//           <p className="mt-3">
+//             This is a Pneumonia Detection application that determines whether an
+//             X-ray image indicates pneumonia or not.
+//           </p>
+//           <div className="mt-5">
+//             <Link to="/upload-image">
+//               <button type="button" className="btn btn-success mr-3">
+//                 Proceed to Upload Image
+//               </button>
+//             </Link>
+//             <Link to="/learn-more">
+//               <button type="button" className="btn btn-info">
+//                 Learn More
+//               </button>
+//             </Link>
+//           </div>
+//         </div>
+//         <div className="col">
+//           <img src={HomeHeader} alt="Home Header" className="img-fluid" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default HomePage;
+
+
+
+
+
+
+
+
 import React from "react";
 import { Link } from "react-router-dom";
 import HomeHeader from "../assets/image.svg";
@@ -6,22 +52,28 @@ function HomePage() {
   return (
     <div className="container text-center">
       <div className="row">
-        <div className="col d-flex flex-column justify-content-center">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
           <h1 className="mt-5 text-uppercase">Pneumonia Detection</h1>
-          <span className="text-center mt-2">
-            This is a Pneumonia Detection application which detects whether the
-            xray has pneumonia or not.
-          </span>
-          <div className="d-flex justify-content-center mt-5">
+          <p className="mt-3">
+            This is a Pneumonia Detection application that determines whether an
+            X-ray image indicates pneumonia or not.
+          </p>
+          <div className="mt-5">
             <Link to="/upload-image">
-              <button type="button" class="btn btn-success">
+              <button type="button" className="btn btn-success mr-3">
                 Proceed to Upload Image
               </button>
             </Link>
+          <a href="https://en.wikipedia.org/wiki/Pneumonia" target="_blank" rel="noopener noreferrer" style={{ padding: '10px' }}  >
+              <button type="button" className="btn btn-info">
+                Learn More
+              </button>
+            </a>
+          
           </div>
         </div>
         <div className="col">
-          <img src={HomeHeader} />
+          <img src={HomeHeader} alt="Home Header" className="img-fluid" />
         </div>
       </div>
     </div>
@@ -29,3 +81,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,93 +1,20 @@
-// /*import React, { useState } from 'react';
-
-// const UploadPage = () => {
-//   const [selectedFile, setSelectedFile] = useState(null);
-
-//   const handleFileChange = (event) => {
-//     setSelectedFile(event.target.files[0]);
-//   };
-
-//   const handleDetectClick = () => {
-//     // Perform the logic for image detection here
-//     if (selectedFile) {
-//       // You can upload the selectedFile to your server for prediction
-//       // Display the result accordingly
-//       // Example: make a POST request using fetch or Axios
-
-//       // Placeholder code for demonstration
-//       const prediction = Math.random() > 0.5 ? 'Pneumonia detected' : 'No pneumonia detected';
-//       alert(prediction);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Upload Page</h2>
-//       <div className="image-upload">
-//         <label htmlFor="upload-input">Select X-ray Image:</label>
-//         <input id="upload-input" type="file" accept="image/*" onChange={handleFileChange} />
-//       </div>
-//       <button className="detect-button" onClick={handleDetectClick} disabled={!selectedFile}>
-//         Detect Pneumonia
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default UploadPage;
-// */
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const UploadPage = () => {
-//   const [file, setFile] = useState(null);
-
-//   const handleFileUpload = async () => {
-//     try {
-//       const formData = new FormData();
-//       formData.append('image', file);
-
-//       const response = await axios.post('http://localhost:4000/process-image', formData);
-
-//       console.log(response.data); // Handle the response from the server here
-
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const handleFileChange = (event) => {
-//     setFile(event.target.files[0]);
-//   };
-
-//   return (
-//     <div>
-//       <input type="file" accept="image/*" onChange={handleFileChange} />
-//       <button onClick={handleFileUpload}>Upload and Process Image</button>
-//     </div>
-//   );
-// };
-
-// export default UploadPage;
-
-//Mathi ko code lai replace garxu ma
-
 import React, { useState } from "react";
 import axios from "axios";
 import Spinner from "../spinner";
+// import HomeHeader from "../assets/Pneumonia.jpg";
 
 const UploadPage = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
   const [result, setResult] = useState("");
-  const [pneumoniaState, setPneumoniaState] = useState("");
+  // const [ setPneumoniaState] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleFileChange = (event) => {
     setImagePreview(null);
     setResult("");
-    setPneumoniaState("");
+    // setPneumoniaState("");
 
     const file = event.target.files[0];
     setSelectedFile(file);
@@ -99,11 +26,11 @@ const UploadPage = () => {
   };
 
   //Call API to detect Pneumonia and set the state as the result
-  const handlePneumoniaDetection = () => {
-    setLoading(true);
-    setPneumoniaState("Pneumonia Detected");
-    setTimeout(() => setLoading(false), 2000);
-  };
+  // const handlePneumoniaDetection = () => {
+  //   setLoading(true);
+  //   // setPneumoniaState("Pneumonia Detected");
+  //   setTimeout(() => setLoading(false), 2000);
+  // };
 
   //Call API to process image and set the state as the result
   const handleDetectClick = () => {
@@ -117,18 +44,20 @@ const UploadPage = () => {
     formData.append("image", selectedFile);
 
     axios
-      .post("http://localhost:4000/process-image", formData)
+      .post("http://localhost:5000/predict", formData)
       .then((response) => {
         setLoading(false);
-        console.log(response.data.message);
-        setResult(response.data);
+        console.log(response.data);
+        setResult(response.data?.data);
       })
       .catch((error) => {
         setLoading(false);
-        console.error("Error:", error);
-        setError("Error Occured");
+        console.log("error", error);
+        if (error.response) setError(error.response.data);
       });
   };
+
+  console.log({ result });
 
   return (
     <>
@@ -167,26 +96,12 @@ const UploadPage = () => {
             >
               Process Image
             </button>
-            {result ? (
+            {result.length ? (
               <>
-                <p>{result.message}</p>
-                {/* Detect button */}
-                <button
-                  type="button"
-                  className="btn btn-success mt-5"
-                  onClick={handlePneumoniaDetection}
-                >
-                  Detect Pneumonia
-                </button>
-                {/* Display result for pneumonia detection */}
-                {pneumoniaState ? (
-                  <>
-                    <h3 className="mt-4">Result:</h3>
-                    <p>{pneumoniaState}</p>
-                  </>
-                ) : (
-                  <></>
-                )}
+                <>
+                  <h3 className="mt-4">Result:</h3>
+                  <p>{result}</p>
+                </>
               </>
             ) : (
               <></>
@@ -200,6 +115,9 @@ const UploadPage = () => {
               <></>
             )}
           </div>
+          {/* <div className="col">
+            <img src={HomeHeader} alt="Home Header" className="img-fluid" />
+          </div> */}
         </div>
       )}
     </>
